fc384d5b16d71f101c6d66be41158ccb7dfd35be
dmichelson <dmichelson@epiqsystems.com>
2018-07-08 23:45:05 -0500

    created KcdcController

---
+++ DannMichelson.CleanCodeApi/Controllers/KcdcController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace DannMichelson.CleanCodeApi.Controllers
+{
+    public class KcdcController : ApiController
+    {
+        public IHttpActionResult GetSessions
+    }
+}
