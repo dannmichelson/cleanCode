e289341157f7cefad77889b7b6255252080bf561
dmichelson <dmichelson@epiqsystems.com>
2018-07-12 09:40:40 -0500

    more prep

---
+++ DirtyCodeApi/DirtyCodeApi/App_Start/XmlCommentsFinder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace DirtyCodeApi
+{
+    public class XmlCommentsFinder
+    {
+        public static string GetXmlComments()
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var commentsFileName = Assembly.GetExecutingAssembly().GetName().Name + ".XML";
+            var commentsFile = Path.Combine(baseDirectory, "bin", commentsFileName);
+
+            return commentsFile;
+        }
+    }
+}
